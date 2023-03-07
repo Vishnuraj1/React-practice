adf33d4d4572cb05147ee73664bb3346d08af2a6
Vishnuraj G <vraj54208@gmail.com>
2023-03-07 12:33:29 +0530

    The folder is set to do new practices

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import './App.css'
-
-import './App.css'
-import Counter from './Counter'
-import { useState } from 'react';
-
-
 function App() {
- const [state,setState] =useState(false)
   return (
-    <div className='App'>
-     <h1 onClick={()=>setState(!state)} style={{cursor:'pointer'}}>Show/Hide</h1>
-   {/* { state ? <Counter/> : null }  if else type method using ternary operator */}
-   { state && <Counter/> } 
-   {/* By using 'and'(&&) operator (and operator works only when both are true)  */}
+    <div>
+   
      
     </div>
   );
 }
 
 export default App;
-
-// Mounting
-// Updating
-// Unmounting
 
