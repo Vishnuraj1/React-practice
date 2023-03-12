65af154ece657ae4551ef88e72aa8b5900be01bb
Vishnuraj G <vraj54208@gmail.com>
2023-03-12 10:38:01 +0530

    Tried button click navigation using useNavigate in tintu-mon code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MainRouter from './Components/ReactRouter/MainRouter';
-
+// import { useNavigate } from 'react-router-dom'
 
 import React from 'react'
 
 const App = () => {
+  // const navigate =useNavigate()
   return (
     <div className='App'>
+      {/* <button onClick={()=>navigate('/about')}>Click</button> */}
       {/* <h1>Welcome app</h1> */}
       <MainRouter/>
     </div>
