e2662121664663b8dcffcb117bfaa313e0e09d3f
Vishnuraj G <vraj54208@gmail.com>
2023-03-13 16:05:37 +0530

    Firebase writing data successful

--- src/App.js
+++ src/App.js
@@ -9,10 +9,9 @@ const App = () => {
     <div className='App'>
      <h1>Welcome Home</h1>
      <button onClick={()=>{
-           Firebase.firestore().collection('products').get().then((snapshot)=>{
-                  snapshot.forEach((obj)=>{
-                    console.log(obj.data())
-                  })
+           Firebase.firestore().collection('products').add({
+            name:"MI",
+            price:1000
            })
      }}>Click me</button>
 
