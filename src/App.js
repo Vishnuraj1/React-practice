5a2f48b6c82a3ff057ada642a5d7d7364d3607d5
Vishnuraj G <vraj54208@gmail.com>
2023-03-13 15:57:27 +0530

    Read data firebase successfully

--- src/App.js
+++ src/App.js
@@ -1,40 +1,20 @@
+import React from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import React from 'react'
-import About from './Components/About';
-import Profile from './Components/Profile';
-import Portfolio from './Components/Portfolio';
-import { useState } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import { AppContext } from './AppContext';
+import Firebase from './Firebase/Config'
 
 const App = () => {
-  const navigate = useNavigate()
-  const [state, setState] = useState(10)
+ 
   return (
     <div className='App'>
-      <button onClick={() =>  navigate('about') }>About</button> &nbsp;&nbsp;
-      <button onClick={() =>  navigate('profile') }>Profile</button> &nbsp;&nbsp;
-      <button onClick={() =>  navigate('portfolio') }>Portfolio</button>
-
-      <AppContext.Provider value={state}>
-
-        <Routes>
-          <Route path='about'>
-            <About></About>
-          </Route>
-
-          <Route path='profile'>
-            <Profile></Profile>
-          </Route>
-
-          <Route path='protfolio'>
-            <Portfolio></Portfolio>
-          </Route>
-        </Routes>
-
-      </AppContext.Provider>
+     <h1>Welcome Home</h1>
+     <button onClick={()=>{
+           Firebase.firestore().collection('products').get().then((snapshot)=>{
+                  snapshot.forEach((obj)=>{
+                    console.log(obj.data())
+                  })
+           })
+     }}>Click me</button>
 
     </div>
   )
