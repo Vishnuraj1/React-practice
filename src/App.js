4f75e7fe40eb2663ce6187ea2816775b046ebb70
Vishnuraj G <vraj54208@gmail.com>
2023-03-13 16:07:57 +0530

    Firebase data deleting successfull

--- src/App.js
+++ src/App.js
@@ -9,9 +9,8 @@ const App = () => {
     <div className='App'>
      <h1>Welcome Home</h1>
      <button onClick={()=>{
-           Firebase.firestore().collection('products').add({
-            name:"MI",
-            price:1000
+           Firebase.firestore().collection('products').doc('nDOVNxnEyTNwP7cuOGO0').delete().then(()=>{
+            console.log("Done")
            })
      }}>Click me</button>
 
