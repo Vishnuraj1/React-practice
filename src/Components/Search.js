8146b3d92bc7698411c6959d18316c465025cc01
Vishnuraj G <vraj54208@gmail.com>
2023-04-27 21:32:00 +0530

    Change

---
+++ src/Components/Search.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Search = () => {
+  
+  return (
+    <div>
+      <input type="text" placeholder=' Search what you want' style={{borderRadius:"10% 10% 10% 10%"}} />
+      
+    </div>
+  )
+}
+
+export default Search
